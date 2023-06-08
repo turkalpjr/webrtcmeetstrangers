91224f9f90c745303e0feb2d27e47e5ddc1c4763
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2023-06-08 23:17:23 +0300

    38 devam ediliyor
    38 devam ediliyor

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ import * as store from './store.js';
 import *  as wss from './wss.js';
 import * as webRTCHandler from './webRTCHandler.js';
 import * as constants from './constants.js';
-
+import { getIncomingCallDialog } from './ui.js';
 //initialization  of socket IO connection
 const socket = io('/');
 wss.registerSocketEvents(socket);
@@ -35,3 +35,5 @@ personalCodeVideoButton.addEventListener('click', () => {
     webRTCHandler.sendPreOffer(callType,calleePersonalCode);
 });
 
+
+getIncomingCallDialog('VIDEO',()=>{},()=>{});
